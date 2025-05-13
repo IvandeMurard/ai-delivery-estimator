1c4653ad6d91b0d572b5a4d06cb76b0affc2da8e
IvandeMurard <ivandemurard@hotmail.fr>
2025-05-13 19:47:24 +0200

    fix: remove all root hooks, only one scoreDetails state in component

---
+++ src/app/estimation-legacy/page.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const EstimationLegacy = () => {
+  const [scoreDetails, setScoreDetails] = useState<any[]>([]);
+
+  // ... rest of the component code ...
+
+  return (
+    // ... rest of the component JSX ...
+  );
+};
+
+export default EstimationLegacy; 
